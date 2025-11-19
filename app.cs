2fdf130929db27029d6ce8d828858dbdc3615e1a
Renicius Pagotto <renicius.pagotto@outlook.com>
2025-11-19 13:41:41 -0800

    Initial commit: minimum .NET 10 API configuration

--- app.cs
+++ app.cs
@@ -6,5 +6,6 @@ app.MapGet("/", () => "Hello World");
 
 app.MapGet("/featureA", () => "This is Feature A");
 app.MapGet("/featureB", () => "This is Feature B");
+app.MapGet("/featureC", () => "This is Feature C");
 
 app.Run();
